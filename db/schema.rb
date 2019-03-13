04b127cd0b730ce6842e67f4731921145b594402
John Paolo Paulino <paolo716@gmail.com>
2019-03-13 16:52:50 +0800

    Integrated technologies mode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_140653) do
+ActiveRecord::Schema.define(version: 2019_03_13_082511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,22 @@ ActiveRecord::Schema.define(version: 2019_03_11_140653) do
     t.text "badge"
   end
 
+  create_table "technolodies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "portfolio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_technolodies_on_portfolio_id"
+  end
+
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "portfolio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -63,4 +79,6 @@ ActiveRecord::Schema.define(version: 2019_03_11_140653) do
   end
 
   add_foreign_key "blogs", "topics"
+  add_foreign_key "technolodies", "portfolios"
+  add_foreign_key "technologies", "portfolios"
 end
