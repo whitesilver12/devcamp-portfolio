22a26b06e062d514e8952c0d5c51621f279223f7
John Paolo Paulino <paolo716@gmail.com>
2019-03-22 21:22:26 +0800

    Refactored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolio"
     def index
         @portfolio_items = Portfolio.all
@@ -25,12 +26,9 @@ class PortfoliosController < ApplicationController
     end
 
     def edit
-      @portfolio_item = Portfolio.find(params[:id])
     end
 
     def update
-      @portfolio_item = Portfolio.find(params[:id])
-    
       respond_to do |format|
         if @portfolio_item.update(portfolio_params)
           format.html { redirect_to portfolios_path, notice: 'The record was successfully updated' }
@@ -41,11 +39,9 @@ class PortfoliosController < ApplicationController
     end
 
     def show
-      @portfolio_item = Portfolio.find(params[:id])
     end
 
     def destroy
-      @portfolio_item = Portfolio.find(params[:id])
       @portfolio_item.destroy
       respond_to do |format|
         format.html { redirect_to portfolios_path, notice: 'Post was remove' }
@@ -60,5 +56,9 @@ class PortfoliosController < ApplicationController
                                         :body, 
                                         technologies_attributes: [:name]
                                       )
+    end
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
     end
 end
